4e7be8384561e90881c436970a957c9c1b91cc12
Alonso Ortega <kaluza.klein13@gmail.com>
2020-07-01 16:25:48 -0500

    cambios

--- js/backend.js
+++ js/backend.js
@@ -1,3 +1,32 @@
+const key =""
+
+function getToken(cbOK,cbErr) {
+    // 1. Crear XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    // 2. Configurar: PUT actualizar archivo
+   xhr.open('GET', "https://users-dasw.herokuapp.com/api/tokenDASW");     
+    //3. añadir header con la clave
+    xhr.setRequestHeader('x-expediente',729622) 
+   // 4. Enviar solicitud        
+   xhr.send();        
+   // 5. Una vez recibida la respuesta del servidor        
+   xhr.onload = function () {        
+       if (xhr.status == 200) { // analizar el estatus de la respuesta HTTP
+        //console.log(xhr.response);
+           let key = JSON.parse(xhr.response).token; //esta es la línea que hay que probar
+           cbOK(key)
+           // Ejecutar algo si todo está correcto
+           console.log(key); // Significa que fue exitoso             
+       } else {           
+           // Ocurrió un error     
+           alert(xhr.status + ': ' + xhr.statusText); // e.g. 404: Not Found
+           cbErr()
+           // ejecutar algo si error 
+       }
+   };
+}
+
+
 function getUser(cbOK,cbErr) {
     // 1. Crear XMLHttpRequest object
     let xhr = new XMLHttpRequest();
@@ -24,21 +53,20 @@ function getUser(cbOK,cbErr) {
    };
 }
 
-function postUser(cbOK,cbErr) {
+function postUser(usuario,key,cbOK,cbErr) {
     // 1. Crear XMLHttpRequest object
     let xhr = new XMLHttpRequest();
     // 2. Configurar: PUT actualizar archivo
    xhr.open('POST', "https://users-dasw.herokuapp.com/api/users");     
     //3. añadir header con la clave
-   xhr.setRequestHeader('Security-key',key) 
+   xhr.setRequestHeader('x-auth',key) 
+   xhr.setRequestHeader("Content-Type", "application/json");
    // 4. Enviar solicitud        
-   xhr.send();        
+   xhr.send(usuario);        
    // 5. Una vez recibida la respuesta del servidor        
    xhr.onload = function () {        
-       if (xhr.status == 200) { // analizar el estatus de la respuesta HTTP
-        //console.log(xhr.response);
-           let datos = JSON.parse(xhr.response); //esta es la línea que hay que probar
-           cbOK(datos)
+       if (xhr.status == 201) { // analizar el estatus de la respuesta HTTP
+            alert("Usuario creado")
            // Ejecutar algo si todo está correcto
            console.log(datos); // Significa que fue exitoso             
        } else {           
